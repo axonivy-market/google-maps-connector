4c306462942e019987aa3b83a9cb93248e8f00a8
Alexander Suter <alexander.suter@axonivy.com>
2021-12-02 13:53:01 +0100

    Add test project

---
+++ google-maps-connector-test/src_test/com/axonivy/connector/google/maps/test/DemoTest.java
@@ -0,0 +1,28 @@
+package com.axonivy.connector.google.maps.test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import ch.ivyteam.ivy.bpm.engine.client.BpmClient;
+import ch.ivyteam.ivy.bpm.engine.client.ExecutionResult;
+import ch.ivyteam.ivy.bpm.engine.client.element.BpmElement;
+import ch.ivyteam.ivy.bpm.engine.client.element.BpmProcess;
+import ch.ivyteam.ivy.bpm.exec.client.IvyProcessTest;
+import ch.ivyteam.ivy.scripting.objects.CompositeObject;
+
+@IvyProcessTest
+class DemoTest {
+  
+  private static final BpmProcess testee = BpmProcess.path("Demo/searchLocation");
+
+  @Test
+  void callProcess(BpmClient bpmClient){
+    BpmElement startable = testee.elementName("start.ivp");
+    var mapDialogMock = BpmElement.pid("1790C4E5BB25B8AA-f3");
+    bpmClient.mock().uiOf(mapDialogMock).withNoAction();
+    ExecutionResult result = bpmClient.start().process(startable).execute();
+    CompositeObject data = result.data().last();
+    assertThat(data).isNotNull();
+  }
+}
